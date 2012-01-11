bb39e86c006b90adcddd4763008f48c340d67624
Roman V. Babenko <romanvbabenko@gmail.com>
2012-01-11 23:27:04 +0200

    [^] Ability monitrc source teamplete name customization has been added

--- libraries/monitrc.rb
+++ libraries/monitrc.rb
@@ -3,17 +3,19 @@ class Chef
     # name        The name of the service.  Looks for a template named NAME.conf
     # variables   Hash of variables to pass to the template
     # reload      Reload monit so it notices the new service.  :delayed (default) or :immediately.
-    def monitrc(name, variables={}, reload = :delayed)
-      log "Making monitrc for: #{name}"
-      template "/etc/monit/conf.d/#{name}.conf" do
+
+    def monitrc(name, variables = {}, options = {})
+      options = { :reload => :delayed, :source => "#{ name }.conf.erb" }.merge(options)
+      log "Making monitrc for: #{ name }"
+      template "/etc/monit/conf.d/#{ name }.conf" do
         owner "root"
         group "root"
         mode 0644
-        source "#{name}.conf.erb"
+        source options[:source]
         variables variables
-        notifies :restart, resources(:service => "monit"), reload
+        notifies :restart, resources(:service => "monit"), options[:reload]
         action :create
       end
     end
   end
-end  
+end
